49824f754f9b108baa717ff81f51562677744267
chakki1234 <chakkirala.a@gmail.com>
2024-03-27 12:53:11 -0400

    nav bar working but need to integrate it

--- apps/ui/src/views/NavigationPane.tsx
+++ apps/ui/src/views/NavigationPane.tsx
@@ -3,20 +3,28 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
+import ListItemButton from '@mui/material/ListItemButton';
 import Typography from '@mui/material/Typography';
+import GroupIcon from '@mui/icons-material/Group';
+import CodeIcon from '@mui/icons-material/Code';
+import ListItemIcon from '@mui/material/ListItemIcon';
+
 
 export default function AlignItemsList() {
-   
-   let navigationPaneInfo = [
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+    let navigationPaneInfo = [
         {
             heading: "Team Creation",
-            description: "To create a team consisting of a developer and a tester for a particular requirement."
+            description: "To create a team consisting of a developer and a tester.",
+            icon: <GroupIcon/>,
+            selected: true
         },
         {
             heading: "Code Completion",
-            description: "Completes the code from where you are stuck"
+            description: "Completes the code from where you are stuck.",
+            icon: <CodeIcon/>,
+            selected: false
         }
     ] 
 
@@ -32,33 +40,40 @@ export default function AlignItemsList() {
 
     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
         <>
-            {navigationPaneInfo.map((info) => (
+            {navigationPaneInfo.map((info, index) => (
                 <>
-                <ListItem alignItems="flex-start">
-                    <ListItemText
-                        primary={                      
-                        <React.Fragment>
-                            <Typography
-                                sx={{ display: 'block', fontWeight: 'bold'}}
-                                component="span"
-                                variant="body2"
-                                color="text.primary">
-                                {info.heading}
-                            </Typography>
-                        </React.Fragment>}
-                        secondary={
-                        <React.Fragment>
-                            <Typography
-                                sx={{ display: 'inline' }}
-                                component="span"
-                                variant="body2"
-                                color="text.secondary">
-                                {info.description}
-                            </Typography>
-                        </React.Fragment>
-                       }/>
-                </ListItem>
-                <Divider variant="middle" component="li" />
+                    <ListItemButton
+                    selected={selectedIndex === index}
+                    onClick={()=>{setSelectedIndex(index)}}>
+                        <ListItem alignItems="flex-start">
+                            <ListItemIcon>
+                                {info.icon}
+                            </ListItemIcon>
+                            <ListItemText
+                                primary={                      
+                                <React.Fragment>
+                                    <Typography
+                                        sx={{ display: 'block', fontWeight: 'bold'}}
+                                        component="span"
+                                        variant="body2"
+                                        color="text.primary">
+                                        {info.heading}
+                                    </Typography>
+                                </React.Fragment>}
+                                secondary={
+                                <React.Fragment>
+                                    <Typography
+                                        sx={{ display: 'inline' }}
+                                        component="span"
+                                        variant="body2"
+                                        color="text.secondary">
+                                        {info.description}
+                                    </Typography>
+                                </React.Fragment>
+                            }/>
+                        </ListItem>
+                    </ListItemButton>
+                    <Divider variant="middle" component="li" />
                 </>
             ))}
         </>
