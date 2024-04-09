094d32f023ea89e3fb511a4c77f8529ddff095ea
Jesse Peng <vijcp@outlook.com>
2024-04-08 22:02:06 -0400

    fix(sandbox): basic error handling (#14)

--- apps/ui/src/views/SettingsView.tsx
+++ apps/ui/src/views/SettingsView.tsx
@@ -10,40 +10,36 @@ import {
   Checkbox,
 } from "@chakra-ui/react";
 import { useState, useRef } from "react";
-import {
-  getApiKey,
-  setApiKey,
-  setLangModel,
-  getModels
-} from '../libs/llm';
+import { getApiKey, setApiKey, setLangModel, getModels } from "../libs/llm";
 import { useCopilot } from "../components";
+import { MODEL_GEMMA_2B } from "../libs/llm/modelGemma2b";
 
 const MODELS = getModels();
 
 export const SettingsView = () => {
-  const timerId = useRef(0);
+  const timerId = useRef(null);
   const [showPass, setShowPass] = useState(false);
   const [key, setKey] = useState(getApiKey());
-  const [mode, setMode] = useState( localStorage.getItem('model') || MODELS[0].key);
+  const [mode, setMode] = useState(
+    localStorage.getItem("model") || MODELS[0].key
+  );
   const { autoSave, setAutoSave } = useCopilot();
-  const saveApiKey = (apiKey)=>{
-    localStorage.setItem('apiKey', apiKey);
-  }
-  const savePrefModel = (model)=>{
-    localStorage.setItem('model', model)
-  }
-  const debounce = (func, delay)=>{
-    return (e)=>{
-      clearTimeout(timerId.current)
+  const saveApiKey = (apiKey) => {
+    localStorage.setItem("apiKey", apiKey);
+  };
+  const savePrefModel = (model) => {
+    localStorage.setItem("model", model);
+  };
+  const debounce = (func, delay) => {
+    return (e) => {
+      clearTimeout(timerId.current);
       let apiKey = e.target.value;
-      timerId.current = setTimeout( 
-        ()=>{
-          func(apiKey)  
-        }
-        ,delay)
-    }
-  }
-  const handleSaveApiKey = debounce(saveApiKey, 2000)
+      timerId.current = setTimeout(() => {
+        func(apiKey);
+      }, delay);
+    };
+  };
+  const handleSaveApiKey = debounce(saveApiKey, 2000);
 
   return (
     <Box p={3}>
@@ -56,23 +52,28 @@ export const SettingsView = () => {
           onChange={(e) => {
             const mode = e.target.value;
             setMode(mode);
-            setLangModel(mode)
+            setLangModel(mode);
             savePrefModel(mode);
           }}
         >
-          {
-            MODELS.map((m) => {
-              return <option key={m.key} value={m.key}>{m.name}</option>
-            })
-          }
+          {MODELS.map((m) => {
+            return (
+              <option key={m.key} value={m.key}>
+                {m.name}
+              </option>
+            );
+          })}
         </Select>
-        <FormLabel paddingTop={4}>{mode.charAt(0).toUpperCase() + mode.slice(1)} key</FormLabel>
+        <FormLabel paddingTop={4}>
+          API key
+        </FormLabel>
         <InputGroup size="md">
           <Input
             pr="4.5rem"
             type={showPass ? "text" : "password"}
             placeholder={`Enter ${mode} api-key`}
             value={key}
+            isDisabled={mode === MODEL_GEMMA_2B.key}
             onChange={(e) => {
               const apiKey = e.target.value;
               setKey(apiKey);
@@ -90,7 +91,14 @@ export const SettingsView = () => {
             </Button>
           </InputRightElement>
         </InputGroup>
-        <Checkbox pt={3} colorScheme='yellow' isChecked={autoSave} onChange={() => setAutoSave(!autoSave)} >Auto Save</Checkbox>
+        <Checkbox
+          pt={3}
+          colorScheme="yellow"
+          isChecked={autoSave}
+          onChange={() => setAutoSave(!autoSave)}
+        >
+          Auto Save
+        </Checkbox>
       </FormControl>
     </Box>
   );
