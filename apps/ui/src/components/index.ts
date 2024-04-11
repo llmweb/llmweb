f71c765c040f64a9fd6631198e9202f509733325
chakki1234 <chakkirala.a@gmail.com>
2024-04-11 00:57:38 -0400

    separeted the navbar

--- apps/ui/src/components/index.ts
+++ apps/ui/src/components/index.ts
@@ -2,3 +2,4 @@ export { CopilotProvider, useCopilot } from "./CopilotProvider";
 export { MermaidDiagram } from "./MermaidDiagram";
 export { MonacoEditor } from "./MonacoEditor";
 export { MaterialIcon } from "./MaterialIcon";
+export { NavigationBar } from "./NavigationBar";
